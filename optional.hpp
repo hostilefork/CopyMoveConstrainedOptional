7ee2ab06f60d84a6fa0f6b4424710c1e78d5c99c
Dani-Hub <daniel.kruegler@googlemail.com>
2014-07-06 23:45:01 +0200

    Adapt copyright

--- optional.hpp
+++ optional.hpp
@@ -1,7 +1,7 @@
 #ifndef OPTIONAL_HPP_INCLUDED
 #define OPTIONAL_HPP_INCLUDED
 
-// Copyright 2011-2013 Daniel Kr&uuml;gler.
+// Copyright 2011-2014 Daniel Kr&uuml;gler.
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
